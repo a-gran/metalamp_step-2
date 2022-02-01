283bf027c898dea3564b9c206648ccc02f5a856d
Aristarh Gran <aristarhgran@gmail.com>
2022-02-01 17:26:00 +0300

    settings webpack

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import './styles/index.scss'
+import $ from "jquery";
+// import 'bootstrap';
 
 const userStack = {
-  language: 'JavaScript',
-  framework: 'Angular',
+    language: 'JavaScript',
+    framework: 'Angular'
 }
 
 const user = {
-  name: 'Aristarh',
-  age: '38',
-  ...userStack,
+    name: 'Vitalij',
+    age: '37',
+    ...userStack
 }
+
+$('.block').html('jQuery is working');
 
 console.log(user)
